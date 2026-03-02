1c90c7f066d522b800e0045d528f65d21dbd9964
elimist07 <elimist63@gmail.com>
2026-03-02 12:52:02 +0545

    how to get top 3 frequent word...??

--- cli_text_analyzer.cpp
+++ cli_text_analyzer.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-//#include <string>
+#include <vector>
 #include <unordered_map>
+#include <algorithm>
 #include <fstream> //for file operations
 using namespace std;
-int count(char **);
+void count(const std::string);
+void frequency(const std::string);
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -11,14 +13,21 @@ int main(int argc, char *argv[])
         std::cout << "Invalid syntax!\nType help for more" << std::endl;
         return 0;
     }
-    count(argv);
+    const std::string user_file = argv[1];
+    count(user_file);
+    frequency(user_file);
     return 0;
 }
-int count(char *argv[])
+void count(const std::string user_file)
 {
-    int ch, White_Space = 1, words = 0, lines = 0, characters = 0;
-    unordered_map<int,int>freq;//key-element
-    std::ifstream myfile(argv[1], std::ios::binary);
+    std::ifstream myfile(user_file, std::ios::binary);
+    if (!myfile.is_open())
+    {
+        cerr << "Error:Cannot open file" << endl;
+        return;
+    }
+    long int ch, White_Space = 1, words = 0, lines = 0, characters = 0;
+
     while ((ch = myfile.get()) != EOF)
     {
         characters++;
@@ -34,10 +43,23 @@ int count(char *argv[])
             White_Space = 1;
         }
     }
-    std::cout <<"No of words= "<< words << std::endl
-              <<"No of lines= "<< lines << std::endl
-              <<"No of characters= "<< characters << std::endl;
+    std::cout << "No of words= " << words << std::endl
+              << "No of lines= " << lines << std::endl
+              << "No of characters= " << characters << std::endl;
 
     myfile.close();
-    return 0;
+}
+void frequency(const std::string user_file)
+{
+    std::fstream file;
+    file.open(user_file.c_str());
+    if (!file.is_open())
+        return;
+    std::unordered_map<std::string, int> frequency;
+    std::string word;
+    while (file >> word)
+    {
+        frequency[word]++;
+    }
+    file.close();
 }
