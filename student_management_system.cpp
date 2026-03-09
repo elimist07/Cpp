fe6e5faba2b80c3873bd3b26e9a436b9f024ba32
elimist07 <elimist63@gmail.com>
2026-03-09 16:13:10 +0545

    In function show added sort then displays data, Added search record, func overloaded inside class for obj id comparison

--- student_management_system.cpp
+++ student_management_system.cpp
@@ -2,18 +2,28 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 using namespace std;
 class Student
 {
 public:
     char name[100];
     int id;
+    /*operator overloading for < using id for comparison.
+    The operator < is a member function of Student.
+    Member functions have direct access to the class's own members(id,name),
+    without needing to specify which object,
+    because they are already inside that object's context.
+    */
+    bool operator<(const Student &other) const
+    {
+        return id < other.id;
+    }
 };
 int add_record(const std::string);
 int delete_record(std::string);
 int search_record(std::string);
 int show(const std::string);
-int sort();
 int main()
 {
     std::string file_name;
@@ -29,24 +39,29 @@ int main()
         {
         case 0:
             return 0;
+
         case 1:
         {
-            add_record(file_name);
+            if (add_record(file_name))
+                return 0;
             break;
         }
         case 2:
         {
-            delete_record(file_name);
+            if (delete_record(file_name))
+                return 0;
             break;
         }
         case 3:
         {
-            search_record(file_name);
+            if (search_record(file_name))
+                return 0;
             break;
         }
         case 4:
         {
-            show(file_name);
+            if (show(file_name))
+                return 0;
             break;
         }
         default:
@@ -76,7 +91,7 @@ int add_record(const std::string file_name)
     return 0;
 }
 
-int show(const std::string file_name)
+int delete_record(std::string file_name)
 {
     std::ifstream user_file(file_name, std::ios::in | std::ios::binary);
     if (!user_file.is_open())
@@ -85,32 +100,14 @@ int show(const std::string file_name)
         return 1;
     }
 
-    Student student;
-    while (user_file.read(reinterpret_cast<char *>(&student), sizeof(Student)))
-    {
-        std::cout << "Name: " << student.name << std::endl;
-        std::cout << "Id: " << student.id << std::endl;
-    }
-
-    user_file.close();
-    return 0;
-}
-
-int delete_record(std::string file_name)
-{
-    std::ifstream user_file(file_name, std::ios::in);
-    if (!user_file.is_open())
-    {
-        return 1;
-    }
-
     std::string temp = "temp.bin";
-    std::ofstream temp_file(temp, std::ios::out);
+    std::ofstream temp_file(temp, std::ios::out | std::ios::binary);
     if (!temp_file.is_open())
     {
+        std::cerr << "Error: Failed to open file" << std::endl;
         return 1;
     }
-    
+
     int id;
     Student student;
     std::cout << "Enter record id you want to delete: ";
@@ -135,10 +132,54 @@ int delete_record(std::string file_name)
 
 int search_record(std::string file_name)
 {
+    std::ifstream user_file(file_name, std::ios::in | std::ios::binary);
+    if (!user_file.is_open())
+    {
+        std::cerr << "Error: Failed to open file" << std::endl;
+        return 1;
+    }
+    int id;
+    std::cout << "Enter Id you want to search: ";
+    std::cin >> id;
+    Student student;
+    while (user_file.read(reinterpret_cast<char *>(&student), sizeof(Student)))
+    {
+        if (student.id == id)
+        {
+            std::cout << "ID FOUND" << std::endl;
+            std::cout << "Id: " << student.id << "\nName: " << student.name << std::endl;
+            user_file.close();
+            return 0;
+        }
+    }
+    std::cout<<"Id does not exist"<<std::endl;
+    user_file.close();
     return 0;
 }
 
-int sort()
+int show(std::string file_name)
 {
+    std::ifstream user_file(file_name, std::ios::in | std::ios::binary);
+    if (!user_file.is_open())
+    {
+        std::cerr << "Error: Failed to open file" << std::endl;
+        return 1;
+    }
+    // does sorting first and then displays data
+    std::vector<Student> student;
+    Student temp;
+    while (user_file.read(reinterpret_cast<char *>(&temp), sizeof(Student)))
+    {
+        student.push_back(temp);
+    }
+
+    std::sort(student.begin(), student.end());
+    for (auto &p : student)
+    {
+        std::cout << "Id: " << p.id << std::endl;
+        std::cout << "Name: " << p.name << std::endl;
+    }
+
+    user_file.close();
     return 0;
 }
