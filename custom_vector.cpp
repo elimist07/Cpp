f04c26c212b373745aad3acd6bd9c341fca5735c
elimist07 <elimist63@gmail.com>
2026-03-12 13:20:38 +0545

    added deconstructor,pop,size decrease..

--- custom_vector.cpp
+++ custom_vector.cpp
@@ -8,36 +8,30 @@ private:
     int *array = NULL;
 
 public:
+    // calls constructor
     DynamicArray()
     {
         array_size = 0;
         capacity = 1;
         array = new int[capacity];
     }
-
+    // calls constructor and gives size of user input
     DynamicArray(int capacity)
     {
         array_size = 0;
         this->capacity = capacity;
         int *array = new int[capacity];
     }
-
+    // inserts element to last position
     void insert(int element)
     {
 
         if (array_size == capacity)
             size_increase();
+
         array[array_size] = element;
         array_size++;
     }
-
-    
-    void show()
-    {
-        for (int a = 0; a < array_size; ++a)
-            std::cout << array[a] << std::endl;
-    }
-
 
     void size_increase()
     {
@@ -52,6 +46,54 @@ public:
         delete[] array;
         array = temp_array;
     }
+
+    // deletes last element of array
+    void pop()
+    {
+        array[array_size - 1] = 0;
+        array_size--;
+        if (array_size == capacity / 2)
+            size_decrease();
+    }
+
+    void size_decrease()
+    {
+        int *temp = new int[capacity / 2];
+        capacity = capacity / 2;
+        for (int i = 0; i < array_size; ++i)
+        {
+            temp[i] = array[i];
+        }
+        delete[] array;
+        array = temp;
+    }
+
+    int getsize()
+    {
+        return array_size;
+    }
+
+    void show()
+    {
+        for (int a = 0; a < array_size; ++a)
+            std::cout << array[a] << std::endl;
+    }
+
+    bool isempty()
+    {
+        if (array_size == 0)
+            return 1;
+        else
+            return 0;
+    }
+    int getcapacity()
+    {
+        return capacity;
+    }
+    //calls deconstructor,its needed to free dyanmically allocated memory above
+    ~DynamicArray(){
+
+    }
 };
 int main()
 {
@@ -59,6 +101,11 @@ int main()
     arr.insert(5);
     arr.insert(6);
     arr.insert(8);
+    arr.insert(10);
+    arr.pop();
     arr.show();
+    cout << "array size="<<arr.getsize()<<endl;
+    cout << "capacity="<<arr.getcapacity()<<endl;
+    cout << arr.isempty()<<endl;
     return 0;
 }
